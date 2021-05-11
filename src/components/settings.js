42fa36c075cd25780af68e6ccc50d03249b7d720
Kaung Myat Thu <2962000kaungmyatthu@gmail.com>
2021-05-12 04:45:10 +0630

    explain why api key isnt hidden

--- src/components/settings.js
+++ src/components/settings.js
@@ -1,4 +1,4 @@
-export const API_KEY = "Fo5SJqhalRdYKBqDqbNDX5VwkKto2RL8mK9174LI";
+export const API_KEY = "Fo5SJqhalRdYKBqDqbNDX5VwkKto2RL8mK9174LI"; //This is a client-side application Couldn't hide api key anyway, so not gonna hide. it
 export const URLS = {
     APOD_ROOT:"/apod"
 }
