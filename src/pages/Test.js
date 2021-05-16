d7182e5507ed8640bf1faf7b14f27c3e6ae9cda7
Kaung Myat Thu <2962000kaungmyatthu@gmail.com>
2021-05-17 05:28:34 +0630

    moved from compoenents folder to pages

---
+++ src/pages/Test.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import FullscreenLoader from '../components/loader/fullscreenloader.component';
+
+export const Test = () => {
+    return (
+        <div className="container">
+            <FullscreenLoader/>
+        </div>
+    )
+}
