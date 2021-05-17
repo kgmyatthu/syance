bb23e27da9421f2679e20fdf2a7324f7a764d7ef
Kaung Myat Thu <2962000kaungmyatthu@gmail.com>
2021-05-18 04:44:59 +0630

    add facebook styled loading animations component

---
+++ src/components/loader/fbloader.component.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import styles from './loader.module.css';
+
+const FBloader = () => {
+    return (
+        <div>
+            <div className={styles.ldsfacebook}><div></div><div></div><div></div></div>
+        </div>
+    )
+}
+
+export default FBloader;
